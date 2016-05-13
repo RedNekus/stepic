0c5125e406435704946a238f92f0c648e0852c76
RedNekus <fighterneko@gmail.com>
2016-05-14 01:17:34 +0300

    test1

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ def server(conn, a):
                 data = conn.recv(1024)
                 if not data or data == 'close': break
                 conn.send(data)
-        conn.close()
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(1)
@@ -14,3 +13,4 @@ while True:
         print('Connection', address)
         t = threading.Thread(target=server, args=(connector, address))
         t.start()
+connector.close()
