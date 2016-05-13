95c4053a7571ad5fe4b8a38cd723b6d6ff421237
RedNekus <fighterneko@gmail.com>
2016-05-14 01:28:45 +0300

    test1

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading
 def server(conn, a):
         while True:
                 data = conn.recv(1024)
-                if not data or data == b'close': break
+                if not data or data == 'close': break
                 conn.send(data)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
