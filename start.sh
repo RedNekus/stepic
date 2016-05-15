d01671b78df71641af7ecb95e5aa5bdd43c34867
RedNekus <fighterneko@gmail.com>
2016-05-16 00:58:01 +0300

    sever start

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ cp -r /home/box/stepic/web /home/box/
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo nginx -t
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart﻿﻿
 /home/box/web sudo gunicorn - b 0.0.0.0:8080 hello:app
-#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-#sudo /etc/init.d/gunicorn restart﻿﻿
