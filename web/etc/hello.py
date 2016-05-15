ae983626e5aad8312bb7e12602aa9cad589d6baa
RedNekus <fighterneko@gmail.com>
2016-05-16 02:19:31 +0300

    sever start

--- web/etc/hello.py
+++ web/etc/hello.py
@@ -6,13 +6,13 @@ CONFIG = {
         "--workers = 16",
         "hello.application")
 }
-def wsgi_application(env, start_response):
+def application(env, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
-    #params = env['QUERY_STRING'].split('&')
-    body = 'HELLO'
-    #for i in params:
-        #key, val = params[i].split('=')
-       # body = body + key + '=' + val + '\n'
+    params = env['QUERY_STRING'].split('&')
+    body = ''
+    for i in params:
+        key, val = params[i].split('=')
+        body = body + key + '=' + val + '\n'
     start_response(status, headers)
     return [body]
