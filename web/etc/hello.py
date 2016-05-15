2b7a25811f15232b80cc5f783fb11710bebea15e
RedNekus <fighterneko@gmail.com>
2016-05-16 02:16:15 +0300

    sever start

--- web/etc/hello.py
+++ web/etc/hello.py
@@ -1,11 +1,11 @@
 CONFIG = {
     "mode": "wsgi",
-    "working_dir": r"/home/box/web/hello.py",
-    "python": r"/usr/bin/python3.4",
+    "working_dir": "/home/box/web/hello.py",
+    "python": "/usr/bin/python3.4",
     "args": (
-        r"--bind=0.0.0.0:8080",
-        r"--workers = 16",
-        r"hello.application")
+        "--bind=0.0.0.0:8080",
+        "--workers = 16",
+        "hello.application")
 }
 def wsgi_application(env, start_response):
     status = '200 OK'
