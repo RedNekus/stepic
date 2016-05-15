ed5620feaf8ce7318a1814cc1267a1d94b5bd1ce
RedNekus <fighterneko@gmail.com>
2016-05-16 00:46:37 +0300

    sever start

--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 cp -r /home/box/stepic/web /home/box/
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo nginx -t
+#sudo nginx -t
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart﻿﻿
---
+++ web/hello.py
@@ -0,0 +1,13 @@
+pythonpath = '/home/box/web'
+bind = '0.0.0.0:8080'
+workers = 4
+def wsgi_application(env, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    params = env['QUERY_STRING'].split('&')
+    body = ''
+    for i in params:
+        key, val = params[i].split('=')
+        body = body + key + '=' + val + '\n'
+    start_response(status, headers)
+    return body
