212d344599d93fc3b47b0ed3e32dc481e75e7713
RedNekus <fighterneko@gmail.com>
2016-05-16 03:01:48 +0300

    sever start

--- web/hello.py
+++ web/hello.py
@@ -1,11 +1,3 @@
-CONFIG = {
-    "mode": "wsgi",
-    "working_dir": "/home/box/web/hello.py",
-    "args": (
-        "--bind=0.0.0.0:8080",
-        "--workers = 16",
-        "hello.app")
-}
 def app(environment, response):
     params=environment["QUERY_STRING"].replace("&","\n")
     headers=[("Content-Type", "text/plain")]
