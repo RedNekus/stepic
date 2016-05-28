853ac9c7ed9ddd1f94e560f41013a26306d4852a
RedNekus <fighterneko@gmail.com>
2016-05-29 02:42:01 +0300

    new

--- etc/hello.py
+++ etc/hello.py
@@ -1,12 +1,2 @@
-CONFIG = {
-  'mode': 'wsgi',
-  'python': '/usr/bin/python3',
-  'working_dir': '/media/sf_web',
-  'args': (
-    '--bind=0.0.0.0:8000',
-    '--workers=2',
-    '--timeout=15',
-    '--log-level=debug',
-    'hello:app',
-  ),
-}
+pythonpath = '/home/box/web'
+bind = "0.0.0.0:8080"
