c618a86d839a0aa40cecbc505b8b38455f26ef32
RedNekus <fighterneko@gmail.com>
2016-05-29 03:35:28 +0300

    new

--- start.sh
+++ start.sh
@@ -8,10 +8,10 @@ sudo nginx -t
 #sudo ln -sf /media/sf_web/etc/hello.py /etc/gunicorn.d/hello.py
 #sudo /etc/init.d/gunicorn restart﻿﻿
 #gunicorn -c /etc/gunicorn.d/hello.py hello
-cd /home/box/web
-sudo gunicorn -b 0.0.0.0:8080 hello:app
-cd ask
-sudo gunicorn ask.wsgi:application --bind 0.0.0.0:8000
+#cd /home/box/web
+#sudo gunicorn -b 0.0.0.0:8080 hello:app
+cd ../web/ask
+gunicorn ask.wsgi:application --bind 0.0.0.0:8000
 
 #gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
 #gunicorn -c /home/box/web/etc/django.py wsgi --daemon
