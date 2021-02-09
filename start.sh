5c559b67e28195e3874b889774d8924fc4e1d996
RedNekus <fighterneko@gmail.com>
2021-02-09 03:57:53 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,4 +25,6 @@ sudo /etc/init.d/nginx restart
 #gunicorn -c /home/box/web/etc/hello.py hello:app --daemon
 #gunicorn -c /home/box/web/etc/django.py wsgi --daemon
 sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+#sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
+cd /home/box/web
+gunicorn --bind 0.0.0.0:8000 ask.wsgi:application
